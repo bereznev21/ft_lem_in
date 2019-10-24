3644df6b41cd648d5e60fd96ecf4477e4fb65f53
Redwyn Poetess <rpoetess@ox-s5.21-school.ru>
2019-10-24 21:37:55 +0300

    1

--- ft_lem_in.c
+++ ft_lem_in.c
@@ -6,7 +6,7 @@
 /*   By: rpoetess <rpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 21:25:03 by rpoetess          #+#    #+#             */
-/*   Updated: 2019/10/23 21:28:33 by rpoetess         ###   ########.fr       */
+/*   Updated: 2019/10/24 21:36:11 by rpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -51,7 +51,7 @@ int		ft_init_struct(t_start *start)
 	return (1);
 }
 
-void	ft_print_matrix(int **map, int width)
+void	ft_print_int_map(int **map, int width)
 {
 	int	i;
 	int	j;
@@ -119,12 +119,93 @@ void	ft_start_algo(t_start *start, t_map *map)
 }
 */
 
+/*
 void	ft_start_algo(t_start *start, t_map *map)
+{
+	int	i;
+	int j;
+
+	i = map->
+}
+*/
+
+void	ft_put_end(int **map_road, int i, int j)
+{
+	int	n;
+
+	n = 0;
+	while (map_road[i][n] > 0)
+		n++;
+	map_road[i][n] = j;
+}
+
+void	ft_wrt_ans_map(t_map *map)
+{
+	int		i;
+	int		j;
+	int matrix[6][6] =
+	{
+	{0, 1, 1, 0, 0, 0},
+	{-1, 0, 0, 0, 1, 0},
+	{-1, 0, 0, 1, 0, 0},
+	{0, 0, -1, 0, 0, 1},
+	{0, -1, 0, 0, 0, 1},
+	{0, 0, 0, -1, -1, 0}
+	};
+
+	i = 0;
+	j = 0;
+	while (i < 5)
+	{
+		while (j < 5)
+		{
+			if (matrix[i][j] == 1)
+			{
+				//ft_src_roads(map, i, j);
+				ft_put_end(map->map_road, i, j);
+			}
+			j++;
+		}
+		j = 0;
+		i++;
+	}
+}
+
+void	ft_print_char_map(char **map, int width)
 {
 	int	i;
 	int	j;
 
-	
+	i = 0;
+	j = 0;
+	while (i < width)
+	{
+		while (j < width)
+		{
+			ft_putchar(map[i][j]);
+			j++;
+		}
+		ft_putchar('\n');
+		j = 0;
+		i++;
+	}
+}
+
+void	ft_take_ans(t_map *map)
+{
+	int	i;
+
+	i = 0;
+	map->map_road = (int**)malloc(sizeof(int*) * map->size);
+	while (i < map->size)
+	{
+		if (!(map->map_road[i] = (int*)malloc(sizeof(int) * (map->size))))
+			return ;
+		i++;
+	}
+	ft_zero_map(map->map_road, map->size);
+	ft_wrt_ans_map(map);
+	//ft_print_int_map(map->map_road, map->size);
 }
 
 int		main(void)
@@ -146,10 +227,15 @@ int		main(void)
 	//printf("%s\n", start->rooms);
 	//printf("%s\n", start->leaks);
 	//printf("%s\n", start->end);
-	ft_crt_map_room(start, map);
-	ft_wrt_map_leaks(start, map);
-	ft_print_matrix(map->matrix, map->size);
-	ft_start_algo(start, map);
+//	ft_crt_map_room(start, map);
+//	ft_wrt_map_leaks(start, map);
+//	ft_print_int_map(map->matrix, map->size);
+	
+
+
+	//ft_start_algo(start, map);
+	ft_putchar('\n');
+	ft_take_ans(map);
 	ft_free_int(map->matrix, map->size);
 	ft_free(start);
 	free(map);
