5cf36ac2a7f21b12ea383ed5c8a67ad80f20231f
Redwyn Poetess <rpoetess@ox-c6.21-school.ru>
2019-11-11 20:50:31 +0300

    fix lines

--- src/lem_in_read.c
+++ src/lem_in_read.c
@@ -6,7 +6,7 @@
 /*   By: rpoetess <rpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 14:12:26 by rpoetess          #+#    #+#             */
-/*   Updated: 2019/11/11 18:19:48 by rpoetess         ###   ########.fr       */
+/*   Updated: 2019/11/11 20:16:17 by rpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -84,8 +84,6 @@ t_matrix	read_roads(int fd, t_array rooms, char **line)
 	t_matrix_init(&aj, rooms.count, rooms.count);
 	while (1)
 	{
-		ft_putstr(*line);
-		ft_putchar('\n');
 		//todo: what if road count is 0 in map
 		if (**line != '#')
 		{
@@ -100,6 +98,8 @@ t_matrix	read_roads(int fd, t_array rooms, char **line)
 		free(*line);
 		if (ft_get_next_line(fd, line) <= 0)
 			break ;
+		ft_putstr(*line);
+		ft_putchar('\n');
 	}
 	//free_(split);
 	return (aj);
--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: rpoetess <rpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/10/09 21:25:03 by rpoetess          #+#    #+#             */
-/*   Updated: 2019/11/11 19:25:24 by rpoetess         ###   ########.fr       */
+/*   Updated: 2019/11/11 19:49:10 by rpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,10 +23,11 @@ void	lem_in_main(int fd)
 	t_matrix paths;
 	int n_paths;
 
+	(void)n_paths;
 	aj = lem_in_read(fd, &lem_in);
-	printf("Start: %d End: %d\n\n", lem_in.start, lem_in.end);
+//	printf("Start: %d End: %d\n\n", lem_in.start, lem_in.end);
 	n_paths = suurballe(&aj, &paths, lem_in.start, lem_in.end);
-	printf("total disjoint paths: %d\n", n_paths);
+//	printf("total disjoint paths: %d\n", n_paths);
 	lem_in_output(paths, aj, lem_in);
 //	t_matrix_del(&aj);
 //	t_matrix_del(&paths);
@@ -37,7 +38,8 @@ int		main(void)
 {
 	int fd;
 
-	fd = open("maps/generated/big2.map", O_RDONLY);
+//	fd = open("maps/generated/big2.map", O_RDONLY);
+	fd = open("test_map", O_RDONLY);
 	lem_in_main(fd);
 	close(fd);
 	return (0);
