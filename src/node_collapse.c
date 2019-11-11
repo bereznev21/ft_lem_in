0fc69a43b038a432668e87546ac6f51c09b404f6
Karissa Sticks <ksticks@ox-c4.21-school.ru>
2019-11-11 21:38:16 +0300

    fix collapse

--- src/node_collapse.c
+++ src/node_collapse.c
@@ -3,25 +3,6 @@
 void t_collapse_add(t_collapse *c, int k)
 {
 	t_array_push(&c->a, &k);
-//	t_matrix_duplicate_row(&c->m, k, c->m.m);
-}
-
-static void t_collapse_old(t_matrix *m, t_matrix *paths)
-{
-	int i;
-	t_matrix r;
-
-	r = t_matrix_mul(paths, m);
-	t_matrix_del(paths);
-	*paths = r;
-	t_matrix_t(m);
-	r = t_matrix_mul(m, paths);
-	t_matrix_del(paths);
-	*paths = r;
-	i = -1;
-	while (++i < paths->n)
-		paths->data[i][i] = 0;
-	t_matrix_del(m);
 }
 
 static void t_collapse_new(t_collapse *c, t_matrix *paths)
@@ -37,8 +18,8 @@ static void t_collapse_new(t_collapse *c, t_matrix *paths)
 		j = paths->m;
 		while(--j >= 0)
 		{
-			paths->data[j][k] = paths->data[j][i + c->size];
-			paths->data[k][j] = paths->data[i + c->size][j];
+			paths->data[j][k] += paths->data[j][i + c->size];
+			paths->data[k][j] += paths->data[i + c->size][j];
 		}
 		paths->m--;
 		paths->n--;
@@ -47,14 +28,11 @@ static void t_collapse_new(t_collapse *c, t_matrix *paths)
 
 void t_collapse_do(t_collapse *c, t_matrix *paths)
 {
-	(void)t_collapse_old;
-//	t_collapse_old(&c->m, paths);
 	t_collapse_new(c, paths);
 }
 
 void t_collapse_init(t_collapse *c, int size)
 {
 	c->size = size;
-//	t_matrix_init_identity(&c->m, size);
 	t_array_init(&c->a, sizeof(int));
 }
