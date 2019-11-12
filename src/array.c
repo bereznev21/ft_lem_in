8dd429edb8caa4c710d3673c8f3543bed9628e71
Karissa Sticks <ksticks@ox-d4.21-school.ru>
2019-11-12 18:08:07 +0300

    norme

--- src/array.c
+++ src/array.c
@@ -13,7 +13,7 @@
 #include "lem_in.h"
 #include "libft.h"
 
-void t_array_init(t_array *a, size_t item_size)
+void		t_array_init(t_array *a, size_t item_size)
 {
 	a->size = 4;
 	a->item_size = item_size;
@@ -21,23 +21,23 @@ void t_array_init(t_array *a, size_t item_size)
 	a->data = malloc(a->size * a->item_size);
 }
 
-void *t_array_get(t_array *a, int i)
+void		*t_array_get(t_array *a, int i)
 {
 	if (i >= a->count)
 		return (0);
 	return (a->data + i * a->item_size);
 }
 
-static void *t_array_extend(t_array *a)
+static void	*t_array_extend(t_array *a)
 {
 	a->size *= 2;
 	a->data = ft_realloc(a->data,
-						 a->count * a->item_size,
-						 a->size * a->item_size);
+				a->count * a->item_size,
+				a->size * a->item_size);
 	return (a->data);
 }
 
-void *t_array_push(t_array *a, void *content)
+void		*t_array_push(t_array *a, void *content)
 {
 	if (a->count == a->size - 1)
 		if (!t_array_extend(a))
@@ -47,7 +47,7 @@ void *t_array_push(t_array *a, void *content)
 	return (a->data + a->item_size * (a->count - 1));
 }
 
-void t_array_del(t_array *a)
+void		t_array_del(t_array *a)
 {
 	free(a->data);
 }
