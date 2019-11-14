97b4e3987ba7f16cda2909d032b5d62ffa0769f3
Redwyn Poetess <rpoetess@ox-d6.21-school.ru>
2019-11-14 17:06:59 +0300

    fix init zero

--- src/matrix_init.c
+++ src/matrix_init.c
@@ -6,7 +6,7 @@
 /*   By: rpoetess <rpoetess@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/11/08 15:42:59 by rpoetess          #+#    #+#             */
-/*   Updated: 2019/11/11 18:38:08 by rpoetess         ###   ########.fr       */
+/*   Updated: 2019/11/14 17:00:49 by rpoetess         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,14 +42,22 @@ void		t_matrix_init_zero(t_matrix *mat, int m, int n)
 {
 	int i;
 	int j;
+	long *p;
 
-	t_matrix_init(mat, m, n);
+	mat->m = m;
+	mat->n = n;
+	mat->m_alloc = round_up(m);
+	mat->n_alloc = round_up(n);
+	mat->data = malloc(sizeof(int *) * mat->m_alloc);
 	i = -1;
-	while (++i < mat->m)
+	while (++i < mat->m_alloc)
 	{
+		mat->data[i] = malloc(sizeof(int) * mat->n_alloc);
+		p = (long*)(mat->data[i]);
 		j = -1;
-		while (++j < mat->n)
-			mat->data[i][j] = 0;
+		while (++j < mat->n / 2)
+			p[j] = 0;
+		mat->data[i][mat->n - 1] = 0;
 	}
 }
 
